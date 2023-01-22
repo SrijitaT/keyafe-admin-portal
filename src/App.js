ee5fc32df996b9a3bc53237f734729f9bcfec054
Chandan Bose <bosechandan21@gmail.com>
2023-01-22 23:36:18 +0530

    Added navbar for admin portal

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Header from "./components/header/header.component";
+import { Container } from "react-bootstrap";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Container>
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+        </div>
+      </BrowserRouter>
+    </Container>
+  );
 }
 
 export default App;
---
+++ src/components/header/header.component.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import "./header.styles.scss";
+import { Link, Redirect } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.css";
+import { Nav, Navbar, NavbarBrand, Dropdown } from "react-bootstrap";
+
+export default function Header() {
+  let listener = null;
+  const [scrollState, setScrollState] = useState("header");
+
+  useEffect(() => {
+    listener = document.addEventListener("scroll", (e) => {
+      var scrolled = document.scrollingElement.scrollTop;
+      if (scrolled >= 60) {
+        setScrollState("header active");
+      } else {
+        setScrollState("header");
+      }
+    });
+    return () => {
+      document.removeEventListener("scroll", listener);
+    };
+  }, []);
+
+  return (
+    <Navbar className={scrollState} expand="sm" variant="light" sticky="top">
+      <NavbarBrand className="logo-container">
+        <Link to="/">
+          <img className="logo" src="/images/logo/keyafe.png" alt="logo" />
+        </Link>
+      </NavbarBrand>
+      <Navbar.Toggle />
+      <Navbar.Collapse>
+        <Nav className="ms-auto nav-menu">
+          <Nav.Link href="signIn">
+            {/* <Link className="option" to="/signin"> */}
+            SIGN IN
+            {/* </Link> */}
+          </Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+}
