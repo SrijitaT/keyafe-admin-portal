c75ca01d6ae43653fc7385b5d6ed3c15d1bd4a3c
Chandan Bose <bosechandan21@gmail.com>
2023-07-18 20:36:33 +0530

    added search by order id functionality

--- src/components/orders/sortable-data/index.js
+++ src/components/orders/sortable-data/index.js
@@ -1,31 +1,38 @@
 import React, { useState } from "react";
-import { Table } from "react-bootstrap";
+import { Table, Row, Col } from "react-bootstrap";
 import OrderItems from "../order-items-body";
+import FormInput from "../../form-input/form-input.component";
 
 const SortableTable = ({ data: OrderItem }) => {
   const [orderData, setOrderData] = useState(OrderItem);
   const [sortConfig, setSortConfig] = useState({ key: null, direction: null });
+  const [searchKeyword, setSearchKeyword] = useState("");
 
   const handleSort = (key, customSort) => {
     let direction = "asc";
 
-    if (sortConfig && sortConfig.direction === "asc") {
+    if (sortConfig.key === key && sortConfig.direction === "asc") {
       direction = "desc";
     }
-    setOrderData(
-      [...OrderItem].sort((a, b) => {
-        if (customSort) {
-          return customSort(a[key], b[key], direction);
-        }
-        return defaultSort(a[key], b[key], direction);
-      })
-    );
+
+    let sortedData = [...orderData];
+
+    sortedData.sort((a, b) => {
+      if (customSort) {
+        return customSort(a[key], b[key], direction);
+      }
+      return defaultSort(a[key], b[key], direction);
+    });
+
+    setOrderData(sortedData);
     setSortConfig({ key, direction });
   };
 
   const defaultSort = (a, b, direction) => {
     if (typeof a === "string" && typeof b === "string") {
-      return direction === "asc" ? a.localeCompare(b) : b.localeCompare(a);
+      a = parseFloat(a.replace(/[^0-9.-]+/g, ""));
+      b = parseFloat(b.replace(/[^0-9.-]+/g, ""));
+      return direction === "asc" ? a - b : b - a;
     } else {
       return direction === "asc" ? a - b : b - a;
     }
@@ -39,27 +46,62 @@ const SortableTable = ({ data: OrderItem }) => {
     return 0;
   };
 
+  const handleSearch = (event) => {
+    const keyword = event.target.value.toLowerCase();
+
+    setSearchKeyword(keyword);
+    const filtered = OrderItem.filter((item) =>
+      item.uid.toLowerCase().includes(keyword)
+    );
+
+    if (keyword === "") {
+      setOrderData(OrderItem); // Show the original list when the keyword is empty
+    } else if (keyword !== "") {
+      setOrderData(filtered);
+    }
+  };
+
   return (
     <>
+      <Row>
+        <Col lg={8}></Col>
+        <Col lg={4}>
+          <FormInput
+            label="Search by order id"
+            type="text"
+            onChange={handleSearch}
+            value={searchKeyword}
+          />
+        </Col>
+      </Row>
       <Table striped bordered hover>
         <thead>
           <tr>
             <th>Order ID</th>
-            <th onClick={() => handleSort("createdAt", dateSort)}>
+            <th
+              onClick={() => handleSort("Order Date", dateSort)}
+              style={{ cursor: "pointer" }}
+            >
               Order Date
             </th>
-            <th onClick={() => handleSort("total_amount_payable")}>
+            <th
+              onClick={() => handleSort("Order Price(in Rs)", defaultSort)}
+              style={{ cursor: "pointer" }}
+            >
               Order Price(in Rs)
             </th>
             <th>Delivery Address</th>
-            <th onClick={() => handleSort("delivery_date", dateSort)}>
+            <th
+              onClick={() => handleSort("Delivery Date", dateSort)}
+              style={{ cursor: "pointer" }}
+            >
               Delivery Date
             </th>
             <th>Delivery Type</th>
           </tr>
         </thead>
         <tbody>
-          {OrderItem.map((item) => {
+          {orderData.map((item) => {
             const { uid } = item;
             return <OrderItems item={item} key={uid} />;
           })}
